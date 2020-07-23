349d3cf1e34351d90a457326bb10fb923ff0c9a6
alejgh <alejandrgh11@gmail.com>
2020-07-23 08:28:48 -0400

    Remove code snippets from readmes

--- src/data_reader.py
+++ src/data_reader.py
@@ -1,9 +1,10 @@
 import json
+import re
 import requests
 
 from requests.exceptions import HTTPError
 
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, NavigableString
 
 from .code_analyser import get_source_code_comments
 from .repo import GitHubIssue, GitHubRepoData
@@ -15,6 +16,13 @@ def _get_repo_info(repo_url):
     tokens = [token for token in repo_url.split('/')
               if token != '']
     return tokens[-2], tokens[-1]
+
+def _is_header(tag):
+    p = re.compile('^h[1-6]$')
+    return p.match(tag)
+
+def _is_list(tag):
+    return tag in ['ul', 'ol']
 
 def _make_github_request(url, token):
     response = requests.get(url, headers={'Authorization': f"token {token}"})
@@ -70,8 +78,11 @@ def get_repo_readme_text(author, repo, token):
     readme_element = soup.find('div', {'id': 'readme'})
     if readme_element is None:
         # readme.txt file
+        print(author, repo)
         readme_element = soup.find('div', {'itemprop': 'text'})
-    return readme_element.text.strip()
+        return readme_element.text.strip()
+    else:
+        return parse_readme_content(readme_element)
 
 def get_repo_issues(author, repo, token):
     issues = _make_github_request(f"{GITHUB_BASE_API}/repos/{author}/{repo}/issues", token)
@@ -79,7 +90,23 @@ def get_repo_issues(author, repo, token):
     return [GitHubIssue(issue['title'], issue['body'],
                         get_issue_comments(issue['comments_url'], token))
             for issue in issues_dict]
-    
+
+def parse_readme_content(readme_element):
+    readme_tokens = []
+    for a in readme_element.article.children:
+        if isinstance(a, NavigableString):
+            continue
+        
+        if a.name == 'pre':
+            continue
+        
+        if _is_header(a.name):
+            readme_tokens.append(a.text + ".")
+        elif _is_list(a.name):
+            readme_tokens += [li.text + "." for li in a if not isinstance(li, NavigableString)]
+        else:
+            readme_tokens.append(a.text)
+    return ' '.join(readme_tokens).strip()
 
 def parse_repo_url(repo_url, token):
     author, repo = _get_repo_info(repo_url)
